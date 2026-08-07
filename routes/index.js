7af28428550567606777190d12c312b224a7ac77
karenmahr <kmahr@byupathway.edu>
2026-08-07 18:25:18 -0300

    added the login and logout function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
-
+const passport = require('passport');
 router.use('/api-docs', require('./swagger'));
+
+router.get('/', (req, res) => {
+    //#swagger.tags=['Hello World']
+    if (req.session.user !== undefined) {
+        res.send(`Logged in as ${req.session.user.displayName || req.session.user.username}`);
+    } else {
+        res.send('Logged out');
+    }
+});
+router.get('/login', passport.authenticate('github'), (req, res) => { });
+
+router.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 router.use('/music', require('./music'));
 router.use('/artists', require('./artists'));
